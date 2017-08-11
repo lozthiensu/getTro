e9d40a5b7b563776867c6c18ce4dc8bc36a1dc61
Quang Bao <quangbao1994@gmail.com>
2017-08-11 14:40:41 +0700

    Final

--- runGetFB.sh
+++ runGetFB.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/troNhanh/getTro
 now=$(date +"%T")
 logFile="logRun.txt"
 echo "Start at : $now" >> $logFile
-python3.6 main.py >> $logFile
+python3.6 /home/troNhanh/getTro/main.py >> $logFile
 now=$(date +"%T")
 echo "End at : $now" >> $logFile
 echo " " >> $logFile
